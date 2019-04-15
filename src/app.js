3dbb17235b25ee29ba41c6167c8d063e9b705b95
milsirius <47065468+milsirius@users.noreply.github.com>
2019-04-15 14:32:21 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -20,6 +20,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
+//app.use('/static', express.static('public'))
+
 
 //middlewares
 app.use(morgan('dev'));
